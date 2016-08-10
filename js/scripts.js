7029bf47495b5c1738926d4127eb1190504dd82b
Aimen Khakwani and Jena SanCartier <jena@>
2016-08-10 13:49:35 -0700

    Add user interface, remove alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,17 @@
 // Calculator
+// Business (back-end) logic:
 var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var subtract = function(number1, number2) {
-	return number1 - number2;
-};
+// User Interface (front-end) logic:
+$(document).ready(function() {
+	$("form#add").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
 
-var multiply = function(numbers1, number2) {
-	return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-	return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
-
-alert(divide(number1, number2));
-
-// Calculate BMI
-
-var bmi = function (answer1, answer2) {
- return (answer1 * 703) / (answer2 * answer2);
-};
-var lbs = parseInt(prompt("What is your weight in lbs?"));
-
-var inch = parseInt(prompt("What is your height in inches?"));
-
-
-alert(bmi(lbs, inch));
-
-// Convert temperature from Celsius to Farenheight
-
-var celsius = parseInt(prompt("Enter a temperature in Celsius."));
-
-var convert = function (input1){
-return (input1 *1.8) + 32;
-};
-alert(convert(celsius));
-
-// Convert temperature from Farenheight to Celsius
-
-var fahrenheit = parseInt(prompt("Enter the temperature in fahrenheit, please."));
-
-var convert = function(input) {
-	return (input - 32) * 1.8;
-};
-
-alert(convert(fahrenheit));
+	});
+});
